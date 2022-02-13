318f14efaa09c018cc795b241f5e519abaa19bf6
Niel Stofberg <niel@stofberg.co.uk>
2022-02-13 12:39:45 +0000

    Had to add the app.UseSpa() to Startup.Configure()

--- BaSbrcWeb/BaSbrcWeb/Startup.cs
+++ BaSbrcWeb/BaSbrcWeb/Startup.cs
@@ -83,6 +83,14 @@ namespace Basbrc
                     name: "default",
                     pattern: "{controller}/{action=Index}/{id?}");
             });
+
+            app.UseSpa(spa =>
+            {
+                // To learn more about options for serving an Angular SPA from ASP.NET Core,
+                // see https://go.microsoft.com/fwlink/?linkid=864501
+                spa.Options.SourcePath = "ClientApp";
+            });
+
         }
     }
 }
